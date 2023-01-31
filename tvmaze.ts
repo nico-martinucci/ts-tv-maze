95a6858b936435f047ba2641d1ae99fa153e66df
Nico Martinucci <domenico.martinucci@gmail.com>
2023-01-30 16:38:06 -0800

    finish exercise

--- tvmaze.ts
+++ tvmaze.ts
@@ -5,6 +5,32 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+const BASE_URL = "https://api.tvmaze.com"
+const PLACEHOLDER_IMAGE_AT_THE_TOP_OF_THE_FILE = "https://innovating.capital/wp-content/uploads/2021/05/vertical-placeholder-image.jpg"
+
+
+interface IApiShow {
+  id: number;
+  name: string;
+  summary: string;
+  image: {
+    medium: string;
+  } | null;
+}
+
+interface IShow {
+  id: number;
+  name: string;
+  summary: string;
+  image: string
+}
+
+interface IEpisode {
+  id: number;
+  name: string;
+  season: string;
+  number: string;
+}
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -12,42 +38,40 @@ const $searchForm = $("#searchForm");
  *    Each show object should contain exactly: {id, name, summary, image}
  *    (if no image URL given by API, put in a default image URL)
  */
-
-async function getShowsByTerm(term) {
+async function getShowsByTerm(term: string): Promise<[]> {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+  const response = await axios.get(`${BASE_URL}/search/shows?q=${term}`);
+  console.log(response.data);
+
+  const showData = response.data.map((result: { show: IApiShow }) => {
+
+    const s = result.show;
+
+    return {
+      id: s.id,
+      name: s.name,
+      summary: s.summary,
+      image: s.image?.medium || PLACEHOLDER_IMAGE_AT_THE_TOP_OF_THE_FILE
     }
-  ]
+  });
+
+  console.log(showData);
+
+  return showData;
 }
 
 
 /** Given list of shows, create markup for each and to DOM */
-
-function populateShows(shows) {
+function populateShows(shows: IShow[]): void {
   $showsList.empty();
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src="${show.image}"
+              alt="${show.name}"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -60,16 +84,16 @@ function populateShows(shows) {
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
-
 async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
+  const term = $("#searchForm-term").val() as string;
   const shows = await getShowsByTerm(term);
 
   $episodesArea.hide();
@@ -85,9 +109,46 @@ $searchForm.on("submit", async function (evt) {
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
  */
+async function getEpisodesOfShow(id: number): Promise<IEpisode[]> {
+  const response = await axios.get(`${BASE_URL}/shows/${id}/episodes`)
 
-// async function getEpisodesOfShow(id) { }
+  const episodesData = response.data.map((e: IEpisode) => ({
+    id: e.id,
+    name: e.name,
+    season: e.season,
+    number: e.number
+  }));
 
-/** Write a clear docstring for this function... */
+  return episodesData;
+}
 
-// function populateEpisodes(episodes) { }
+/** Adds list of episodes to the DOM, displaying their name, season, and
+ * episode.
+ */
+function populateEpisodes(episodes: IEpisode[]): void {
+  $episodesArea.empty();
+
+  for (let e of episodes) {
+    let $episode = $(`
+      <li>
+        ${e.name} (season ${e.season}, episode ${e.number})
+      </li>
+    `)
+
+    $episodesArea.append($episode);
+  }
+
+  $episodesArea.show();
+}
+
+/** Controller function to fetch episodes and append them to the DOM. */
+async function getAndShowEpisodes(event: JQuery.ClickEvent): Promise<void> {
+  event.preventDefault();
+
+  const $button = $(event.target);
+  const showId = $button.closest(".Show").data("show-id");
+  const episodes = await getEpisodesOfShow(showId);
+  populateEpisodes(episodes);
+}
+
+$showsList.on("click", ".Show-getEpisodes", getAndShowEpisodes);
